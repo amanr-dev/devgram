51553b526022a894d8f54403d077cd213cf06397
Aman <dv451197@gmail.com>
2024-06-26 18:49:47 +0530

    changes text-color

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div>
-      <h2 className="text-2xl text-white ">
+      <h2 className="text-2xl ">
         A Social Medial App for <span className="text-blue-500"> Devs </span>
         and
         <span className="text-blue-500"> HR's</span>
