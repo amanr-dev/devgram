a674af6f98dfe7f5c1efa1ea64b4e6779f5cf98e
Aman <dv451197@gmail.com>
2024-06-26 19:00:08 +0530

    added all the types

--- utils/types.ts
+++ utils/types.ts
@@ -1,4 +1,4 @@
-// import {User, Session} from 'next-auth'
+import { User, Session } from "next-auth";
 
 export type FormState = {
   title: string;
@@ -8,6 +8,50 @@ export type FormState = {
   githubUrl: string;
   category: string;
 };
+
+export interface ProjectInterface {
+  title: string;
+  description: string;
+  image: string;
+  liveSiteUrl: string;
+  githubUrl: string;
+  category: string;
+  id: string;
+  createdBy: {
+    name: string;
+    email: string;
+    avatarUrl: string;
+    id: string;
+  };
+}
+
+export interface UserProfile {
+  id: string;
+  name: string;
+  email: string;
+  description: string | null;
+  avatarUrl: string;
+  githubUrl: string | null;
+  linkedinUrl: string | null;
+  projects: {
+    edges: { node: ProjectInterface }[];
+    pageInfo: {
+      hasPreviousPage: boolean;
+      hasNextPage: boolean;
+      startCursor: string;
+      endCursor: string;
+    };
+  };
+}
+
+export interface SessionInterface extends Session {
+  user: User & {
+    id: string;
+    name: string;
+    email: string;
+    avatarUrl: string;
+  };
+}
 
 export interface ProjectForm {
   title: string;
