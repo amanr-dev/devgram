d9a5891ded3ec48766f5e768e9780a40e4447b8a
Aman <dv451197@gmail.com>
2024-07-02 21:26:08 +0530

    re write schemas

--- grafbase.config.ts
+++ grafbase.config.ts
@@ -6,28 +6,6 @@ import { graph, config } from "@grafbase/sdk";
 
 const g = graph.Standalone();
 
-
-
-
-type User @model {
-  name: String!,
-  email: String!,
-  avatarUrl: URL,
-  description: String!,
-  githubUrl: URL
-  linkedInUrl: URL
-  projects: URL
- 
-}
-
-
-
-//    g.query('invoiceByNumber', {
-//      args: { invoiceNumber: g.String() },
-//      returns: g.ref(Invoice).list(),
-//      resolver: 'invoice/byNumber',
-//    })
-
 export default config({
   schema: g,
 });
