36b0f19fa46435fec3d87ec0bf1568211177ff9e
Aman <dv451197@gmail.com>
2024-07-05 17:02:05 +0530

    schema writing is changed in new grafbase

--- grafbase.config.ts
+++ grafbase.config.ts
@@ -1,21 +1,14 @@
 import { graph, config } from "@grafbase/sdk";
 
 // Welcome to Grafbase!
-//
 // Configure authentication, data sources, resolvers and caching for your GraphQL API.
 
 const g = graph.Standalone();
 
 const User = g.type("User", {
-  name: g.string(),
-  email: g.string().unique(),
-  avatarUrl: g.url(),
-  description: g.string(),
-  githubUrl: g.url(),
-  linkedInUrl: g.url(),
-  projects: 
+  
 });
 
 export default config({
-  schema: g,
+
 });
