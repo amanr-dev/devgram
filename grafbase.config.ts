41b5f67dfd470a2834d4e5cdd31a4b01925f52b7
Aman <dv451197@gmail.com>
2024-07-06 23:01:52 +0530

    removing grafbase from this project

--- grafbase.config.ts
+++ grafbase.config.ts
@@ -1,30 +1,30 @@
-import { graph, config } from "@grafbase/sdk";
+// import { graph, config } from "@grafbase/sdk";
 
-const g = graph.Standalone();
+// const g = graph.Standalone();
 
-const project = g.type("Project", {
-  title: g.string(),
-  description: g.string(),
-  image: g.url(),
-  liveSiteUrl: g.url(),
-  githubUrl: g.url(),
-  category: g.string().shareable(),
-  createdBy: g.ref(),
-});
+// const project = g.type("Project", {
+//   title: g.string(),
+//   description: g.string(),
+//   image: g.url(),
+//   liveSiteUrl: g.url(),
+//   githubUrl: g.url(),
+//   category: g.string().shareable(),
+//   createdBy: g.ref(user),
+// });
 
-const user = g.type("User", {
-  name: g.string(),
-  email: g.string(),
-  avatarUrl: g.url(),
-  description: g.string().optional(),
-  githubUrl: g.url().optional(),
-  linkedInUrl: g.url().optional(),
-  projects: g.ref(project),
-});
+// const user = g.type("User", {
+//   name: g.string(),
+//   email: g.string(),
+//   avatarUrl: g.url(),
+//   description: g.string().optional(),
+//   githubUrl: g.url().optional(),
+//   linkedInUrl: g.url().optional(),
+//   projects: g.ref(project),
+// });
 
-export default config({
-  graph: g,
-});
+// export default config({
+//   graph: g,
+// });
 
 // Example doc link
 // https://grafbase.com/guides/mongodb
