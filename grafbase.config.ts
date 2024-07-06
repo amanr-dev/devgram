d15a026abfe429bcbc9ee0ab4d8cf1996d55ed9b
Aman <dv451197@gmail.com>
2024-07-06 10:33:33 +0530

    having problem to create ref

--- grafbase.config.ts
+++ grafbase.config.ts
@@ -5,18 +5,24 @@ import { graph, config } from "@grafbase/sdk";
 
 const g = graph.Standalone();
 
-const User = g.type("User", {
+const project = g.type("Project", {
+  title: g.string(),
+  description: g.string(),
+  image: g.url(),
+  liveSiteUrl: g.url(),
+  githubUrl: g.url(),
+  category: g.string().shareable(),
+  createdBy: g.ref(),
+});
+
+const user = g.type("User", {
   name: g.string(),
   email: g.string(),
   avatarUrl: g.url(),
   description: g.string().optional(),
   githubUrl: g.url().optional(),
   linkedInUrl: g.url().optional(),
-  projects: g.ref(Project),
-});
-
-const Project = g.type("Project", {
-  title: g.string(),
+  projects: g.ref(project),
 });
 
 export default config({
