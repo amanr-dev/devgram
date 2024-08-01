0dbd5493b53e27089fe6f9440325dad00f274a10
Aman <dv451197@gmail.com>
2024-08-01 22:15:28 +0530

    removed unused import

--- auth.ts
+++ auth.ts
@@ -1,6 +1,5 @@
 import NextAuth from "next-auth";
 import CredentialsProvider from "next-auth/providers/credentials";
-import { decode } from "jsonwebtoken";
 
 export default NextAuth({
   providers: [
