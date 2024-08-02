d6c7b53a097a91abc38ae4e189fcf7198f947ac3
Aman <dv451197@gmail.com>
2024-08-02 10:21:12 +0530

    adding simple authentication

--- auth.ts
+++ auth.ts
@@ -4,34 +4,27 @@ import CredentialsProvider from "next-auth/providers/credentials";
 export default NextAuth({
   providers: [
     CredentialsProvider({
-      // The name to display on the sign-in form (e.g., 'Sign in with...')
       name: "Credentials",
       credentials: {
         username: { label: "Username", type: "text", placeholder: "jsmith" },
         password: { label: "Password", type: "password" },
       },
       authorize: async (credentials) => {
-        // You need to provide a function here that will validate the credentials
-        // Replace this with your own authentication logic
+        const user = { id: 1, name: "John Smith", email: "jsmith@example.com" };
 
-        const user = { id: 1, name: "John Smith", email: "jsmith@example.com" }; // Example user
-
-        // Implement your own authentication logic here
         if (
           credentials.username === "jsmith" &&
           credentials.password === "password"
         ) {
-          // If authentication is successful, return the user object
           return Promise.resolve(user);
         } else {
-          // If authentication fails, return null
           return Promise.resolve(null);
         }
       },
     }),
   ],
   pages: {
-    signIn: "/auth/signin", // Define your custom sign-in page here
+    signIn: "/auth/signin",
   },
   callbacks: {
     async jwt({ token, user }) {
@@ -40,12 +33,11 @@ export default NextAuth({
       }
       return token;
     },
-    // async session({session, token}) {
-    //   if (!session) {
-    //     throw new Error('No session found');
-    //   }
-    //   session.user.id = token.id;
-    //   return session;
-    // }
+    async session({ session, token }) {
+      if (token) {
+        session.user.id = token.id;
+      }
+      return session;
+    },
   },
 });
