03e3919d51a699075c821c6861a1f63211d88e40
Aman <dv451197@gmail.com>
2024-08-04 23:07:05 +0530

    update

--- auth.ts
+++ auth.ts
@@ -2,42 +2,5 @@ import NextAuth from "next-auth";
 import CredentialsProvider from "next-auth/providers/credentials";
 
 export default NextAuth({
-  providers: [
-    CredentialsProvider({
-      name: "Credentials",
-      credentials: {
-        username: { label: "Username", type: "text", placeholder: "jsmith" },
-        password: { label: "Password", type: "password" },
-      },
-      authorize: async (credentials) => {
-        const user = { id: 1, name: "John Smith", email: "jsmith@example.com" };
-
-        if (
-          credentials.username === "jsmith" &&
-          credentials.password === "password"
-        ) {
-          return Promise.resolve(user);
-        } else {
-          return Promise.resolve(null);
-        }
-      },
-    }),
-  ],
-  pages: {
-    signIn: "/signin",
-  },
-  callbacks: {
-    async jwt({ token, user }) {
-      if (user) {
-        token.id = user.id;
-      }
-      return token;
-    },
-    async session({ session, token }) {
-      if (token) {
-        session.user.id = token.id;
-      }
-      return session;
-    },
-  },
+  providers: [],
 });
